aa50bd61ae38954aee8ab0b1d1879d03f999895e
Sam King <samuel.eric.king@gmail.com>
2023-02-22 12:50:40 -0600

    Add Dealer Class

--- classes.py
+++ classes.py
@@ -26,3 +26,39 @@ class Deck:
         dealt = self.cards[0]
         self.cards = self.cards[1:]
         return dealt
+
+class Dealer:
+    def __init__(self):
+        pass
+
+    def greeting(self):
+        greeting = random.choice(['Changing $600!',
+                                  'You don\'t know when to quit, do ya Griswold?',
+                                  'Why don\'t you give me half the money your were gonna bet, then we\'ll go out back, I\'ll kick you in the nuts, and we\'ll call it a day!',
+                                  'Big bet for a BIG man... Sure you don\'t want to save a few bucks for the buffet?',
+                                  ])
+        print('')
+        print(greeting)
+        print('')
+
+    def dialogue_winner(self):
+        winner_message = random.choice(['18, 27, 35... dealer busts! Looks like you all win again!',
+                                        'Winner, winner - Crisp dinner!',
+                                        'Winner! Now go celebrate with a round of frosty chocolate milkshakes!',
+                                        'You\'ve won, but at what cost?'
+                                        ])
+
+        print('')
+        print(winner_message)
+        print('')
+
+    def dialogue_loser(self):
+        loser_message = random.choice(['You lose, stanger',
+                                       'You tried your best, and you failed miserably. The lesson is: Never try.',
+                                       'You get nothing! You lose! Good day sir! ',
+                                       'You\'ve lost is all. You got greedy, Martin.',
+                                       '... And it\'s gone!',
+                                       ])
+
+        print(loser_message)
+        print('')
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import random
-from classes import Deck
+from classes import Deck, Dealer
 
 deck = Deck()
+dealer = Dealer()
+
+dealer.greeting()
 
 a = deck.deal()  # deal first board card
 b = deck.deal()  # deal second board card
@@ -10,9 +13,10 @@ if a.value_true > b.value_true:  # swap so lowest card is shown first
     a, b = b, a
 
 print(' - ' + str(a.value) + a.suit + ' - ' + str(b.value) + b.suit + ' - ')  # print board
+print('')  # print new line
 
 if a.value_true == b.value_true or b.value_true - a.value_true == 1:  # auto-lose conditions
-    print('You lose, loser!')
+    dealer.dialogue_loser()
 else:
     action = input('Bet or Fold?')
     counter = 0
@@ -24,15 +28,15 @@ else:
         counter += 1
         action = input('Bet or Fold?')
     if action.lower() == 'fold':
-        print('You lose, stanger')
+        dealer.dialogue_loser()
     if action.lower() == 'bet':
         c = deck.deal()
         if c.value_true > a.value_true and c.value_true < b.value_true:
             print(' - ' + str(a.value) + a.suit + ' ' + str(c.value) + c.suit + ' ' + str(b.value) + b.suit + ' - ')
-            print('You win... bigly')
+            dealer.dialogue_winner()
         elif c.value_true <= a.value_true:
             print(str(c.value) + c.suit + ' ' + str(a.value) + a.suit + ' - ' + str(b.value) + b.suit + ' - ')
-            print("You shouldnt'a did that... he's just a boy")
+            dealer.dialogue_loser()
         else:
             print(' - ' + str(a.value) + a.suit + ' - ' + str(b.value) + b.suit + ' ' + str(c.value) + c.suit)
-            print('Oops... sorry')
+            dealer.dialogue_loser()
